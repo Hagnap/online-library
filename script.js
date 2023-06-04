c5888537bb5d4ad55a05b223720c4120b2671404
jhagg26 <jhagg26@gmail.com>
2023-06-03 20:34:06 -0400

    Make cancel form function and hiding the form dynamic

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 /* Store DOM element references */
 const addBookBtn = document.querySelector("#add-book-btn");
+const submitFormBtn = document.querySelector("#submit-btn");
+const cancelFormBtn = document.querySelector("#cancel-btn");
+
 const formContainer = document.querySelector(".form-container");
+const form = document.querySelector("form");
+
+let displayForm = false;
+
+/* Util functions */
+function hideForm() {
+    formContainer.style.display = "none";
+    form.reset();
+    displayForm = false;
+}
+
+function showForm() {
+    formContainer.style.display = "flex"
+    displayForm = true;
+}
 
 /* Add EventListeners */
 addBookBtn.addEventListener("click", (event) => {
-    formContainer.style.display = "flex";
+    //toggle form display
+    if(!displayForm) {
+        showForm();   
+    }
+    else {
+        hideForm();
+    }
+
     console.log("Click!");
+});
+
+cancelFormBtn.addEventListener("click", (event) => {
+
+
+    formContainer.style.display = "none";
 });
